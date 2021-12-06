1eb3d49833baa24f3e69fc04e0d51026a5263e48
Maria Daan <mariadaan@gmail.com>
2021-12-06 18:26:07 +0100

    part 1

--- Day_04/day4.py
+++ Day_04/day4.py
@@ -1,4 +1,18 @@
-# 
+
+def board_has_bingo(board):
+	for row in board:
+		hor_sum = 0
+		for number in row:
+			hor_sum += number
+		if hor_sum == -5:
+			return 1
+	for j in range(5):
+		vert_sum = 0
+		for i in range(5):
+			vert_sum += board[i][j]
+		if vert_sum == -5:
+			return 1
+	return 0
 
 def is_bingo(all_boards):
 	# for one_board in all_boards:
@@ -10,21 +24,26 @@ def is_bingo(all_boards):
 	vert_sum = 0
 	board_num = 0
 	for board in all_boards:
-		for row in board:
-			hor_sum = 0
-			for number in row:
-				hor_sum += number
-		for j in range(5):
-			vert_sum = 0
-			for i in range(5):
-				vert_sum += board[i][j]
-		if hor_sum == -5 or vert_sum == -5:
-			for testrow in board:
-				print(testrow)
-			print("BINGO!")
+		if board_has_bingo(board):
+			print("BOARD NUM: ", board_num)
 			return board_num
 		board_num += 1
 	return -1
+
+def not_bingo_board(all_boards):
+	count = 0
+	for board in all_boards:
+		if board_has_bingo(board):
+			count += 1
+		if count == 99:
+			return board
+	return -1
+
+def all_boards_bingo(all_boards):
+	for board in all_boards:
+		if board_has_bingo(board) == 0:
+			return 0
+	return 1
 
 def calc_result(board):
 	total = 0
@@ -43,21 +62,16 @@ first_line = bingo[0]
 balls = first_line.split(",")
 balls = [int(x) for x in balls]
 boards = bingo[2:]
-print("balls: ", balls)
-print("\nboards: ", boards)
-
 all_boards = []
 len_boards = len(boards)
 j = 0
 while j < len_boards:
 	one_board = []
-	print("\nboard")
 	for i in range(5):
 		line = boards[i + j]
 		line = line.replace("  ", " ")
 		line = line.strip()
 		line = line.split(" ")
-		print(line)
 		numbers = [int(x) for x in line]
 		one_board.append(numbers)
 	all_boards.append(one_board)
@@ -83,23 +97,39 @@ for ball in balls:
 				i_numbers += 1
 			i_rows += 1
 		i_boards += 1
+		# if all_boards_bingo(all_boards):
+		# 	print("all boards bingo!")
+		# 	total = calc_result(all_boards[board_num])
+		# 	print("\nboard num: ", board_num)
+		# 	print("winning board: ", all_boards[board_num])
+		# 	print("winning_num:   ", ball)
+		# 	final_result = total * ball
+		# 	print("final result: ", final_result)
+		# 	exit()
+
 	board_num = is_bingo(all_boards)
 	if board_num != -1:
 		total = calc_result(all_boards[board_num])
-		print("board num: ", board_num * 6)
+		print("\nboard num: ", board_num)
 		print("winning board: ", all_boards[board_num])
 		print("winning_num:   ", ball)
 		final_result = total * ball
 		print("final result: ", final_result)
+
+		# del all_boards[board_num]
 		exit()
+
 	i_balls += 1
 
-print(all_boards)
+# print("winning_num:   ", 10)
+# total = calc_result(all_boards[90])
+# final_result = total * 10
+# print("final result: ", final_result)
 
-# 61 87 90 15 17
-# 22 82 28 21 93
-# 65 98 12 23 24
-# 73 70 42  1 94
-# 83 79  5 18 55
+# 99 54 74 83 92
+# 27 53 15  8 85
+# 94 36 63 29 91
+# 58 10 45 38 79
+#  9 95 23 98 33
 
 # 67,31,58,8,79,18,19,45,38,13,40,62,85,10,21,96,56,55,4,36,76,42,32,34,39,89,6,12,24,57,93,47,41,52,83,61,
