6c372d0a813a0e9ae6eb20d21ea0a4acf4930902
Maria Daan <mariadaan@gmail.com>
2021-12-13 18:38:41 +0100

    troep

--- Day_11/day11.py
+++ Day_11/day11.py
@@ -1,4 +1,5 @@
 import copy
+import sys
 
 def print_2D(double_array):
 	""" Print 2D array """
@@ -51,56 +52,67 @@ def increment_all(grid, value):
 		for i_num in range(1, grid_width - 1):
 			grid[i_row][i_num] += value
 
-def change_all(grid, condition, value):
-	grid_height = len(grid)
-	grid_width = len(grid[0])
-	for i_row in range(1, grid_height - 1):
-		for i_num in range(1, grid_width - 1):
-			if grid[i_row][i_num] >= condition:
-				grid[i_row][i_num] = value
+# def change_all(grid, condition, value):
+# 	grid_height = len(grid)
+# 	grid_width = len(grid[0])
+# 	for i_row in range(1, grid_height - 1):
+# 		for i_num in range(1, grid_width - 1):
+# 			if grid[i_row][i_num] >= condition:
+# 				grid[i_row][i_num] = value
+
+def surrounders(grid, x, y):
+	if grid[y][x - 1] != 0 and grid[y][x - 1] != -1:
+		grid[y][x - 1] += 1
+	if grid[y - 1][x - 1] != 0 and grid[y - 1][x - 1] != -1:
+		grid[y - 1][x - 1] += 1
+	if grid[y - 1][x] != 0 and grid[y - 1][x] != -1:
+		grid[y - 1][x] += 1
+	if grid[y - 1][x + 1] != 0 and grid[y - 1][x + 1] != -1:
+		grid[y - 1][x + 1] += 1
+	if grid[y][x + 1] != 0 and grid[y][x + 1] != -1:
+		grid[y][x + 1] += 1
+	if grid[y + 1][x + 1] != 0 and grid[y + 1][x + 1] != -1:
+		grid[y + 1][x + 1] += 1
+	if grid[y + 1][x] != 0 and grid[y + 1][x] != -1:
+		grid[y + 1][x] += 1
+	if grid[y + 1][x - 1] != 0 and grid[y + 1][x - 1] != -1:
+		grid[y + 1][x - 1] += 1
 
 def flash(grid, x, y):
-	grid[y][x - 1] += 1
-	grid[y - 1][x - 1] += 1
-	grid[y - 1][x] += 1
-	grid[y - 1][x + 1] += 1
-	grid[y][x + 1] += 1
-	grid[y + 1][x + 1] += 1
-	grid[y + 1][x] += 1
-	grid[y + 1][x - 1] += 1
+	grid[y][x] = 0
+	surrounders(grid, x, y)
+	print_2D(grid)
+	if grid[y][x - 1] >= 10:
+		flash(grid, y, x - 1)
+	if grid[y - 1][x - 1] >= 10:
+		flash(grid, y - 1, x - 1)
+	if grid[y - 1][x] >= 10:
+		flash(grid, y - 1, x)
+	if grid[y - 1][x + 1] >= 10:
+		flash(grid, y - 1, x + 1)
+	if grid[y][x + 1] >= 10:
+		flash(grid, y, x + 1)
+	if grid[y + 1][x + 1] >= 10:
+		flash(grid, y + 1, x + 1)
+	if grid[y + 1][x] >= 10:
+		flash(grid, y + 1, x)
+	if grid[y + 1][x - 1] >= 10:
+		flash(grid, y + 1, x - 1)
+
 
 def increment_adjacents(grid):
-	newflash = 0
 	grid_height = len(grid)
 	grid_width = len(grid[0])
 	for y in range(1, grid_height - 1):
 		for x in range(1, grid_width - 1):
-			if grid[y][x] == 0 or grid[y][x] >= 10:
-				continue
-			left = grid[y][x - 1]
-			upleft = grid[y - 1][x - 1]
-			up = grid[y - 1][x]
-			upright = grid[y - 1][x + 1]
-			right = grid[y][x + 1]
-			downright = grid[y + 1][x + 1]
-			down = grid[y + 1][x]
-			downleft = grid[y + 1][x - 1]
-			adjacents = [left, upleft, up, upright, right, downright, down, downleft]
-			flash_count = adjacents.count(0)
-			grid[y][x] += flash_count
 			if grid[y][x] >= 10:
-				newflash += 1
-				# grid[y][x] = 0
-	change_all(grid, 10, 0)
-	return newflash
-
+				print("back")
+				flash(grid, x, y)
 
 def take_step(grid):
 	increment_all(grid, 1)
-	change_all(grid, 10, 0)
-	newflash = 1
-	while newflash:
-		newflash = increment_adjacents(grid)
+	# change_all(grid, 10, 0)
+	increment_adjacents(grid)
 	flashes = count_2D(grid, 0)
 	return flashes
 
@@ -109,14 +121,15 @@ def simulate_steps(grid, steps):
 	for step in range(steps):
 		flashes += take_step(grid)
 		print(f"After step {step + 1}")
-		print_2D_noborders(grid)
+		print_2D(grid)
 	return flashes
 
 if __name__ == "__main__":
+	sys.setrecursionlimit(500)
 	octos = load_data("sample.txt")
 	octos_bordered = add_borders(octos, -1)
 	print("Before any steps:")
-	print_2D_noborders(octos_bordered)
-	steps = 6
+	print_2D(octos_bordered)
+	steps = 2
 	flashes = simulate_steps(octos_bordered, steps)
 	print("flashes: ", flashes)
