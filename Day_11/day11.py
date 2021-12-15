9dfb0d5a78dfb45b3d002e3e078497cafb90d69a
Maria Daan <mariadaan@gmail.com>
2021-12-15 16:22:12 +0100

    flashes

--- Day_11/day11.py
+++ Day_11/day11.py
@@ -1,135 +1,82 @@
-import copy
-import sys
-
-def print_2D(double_array):
-	""" Print 2D array """
-	for row in double_array:
-		print(row)
-	print()
-
-def print_2D_noborders(grid):
-	""" Print 2D array """
-	grid_height = len(grid)
-	grid_width = len(grid[0])
-	for i_row in range(1, grid_height - 1):
-		for i_num in range(1, grid_width - 1):
-			print(grid[i_row][i_num], end = '')
-		print()
-	print()
+import numpy as np
+from itertools import product
 
 def load_data(filename):
-	""" Loads data as 2d int array """
-	file = open(filename, 'r')
-	data = file.read().splitlines()
-	for index, line in enumerate(data):
-		data[index] = list(line)
-		data[index] = [int(x) for x in data[index]]
+	""" Load data as 2D int numpy array"""
+	with open(filename) as file:
+		raw_data = file.read().strip().split("\n")
+	data = np.array([[int(x) for x in list(row)] for row in raw_data])
 	return data
 
-def add_borders(grid, pad):
-	""" Add a padding around the whole grid """
-	framed = copy.deepcopy(grid)
-	grid_height = len(framed)
-	n = len(framed[0]) + 2
-	top_bottom = [pad] * n
-	for i in range(grid_height):
-		framed[i].insert(0, pad)
-		framed[i].append(pad)
-	framed.insert(0,top_bottom)
-	framed.append(top_bottom)
-	return framed
+def increment_all(grid, height, value):
+	""" Increment all numbers in grid by value """
+	for y, x in product(range(height), repeat=2):
+		grid[y, x] += value
 
-def count_2D(grid, value):
-	count = 0
-	for row in grid:
-		count += row.count(value)
-	return count 
+def take_step(grid, height):
+	flashes = 0
+	# create new array with zeroes to save flashed octos
+	flashed = np.zeros((height, height), dtype=bool)
+	increment_all(grid, height, 1)
 
-def increment_all(grid, value):
-	grid_height = len(grid)
-	grid_width = len(grid[0])
-	for i_row in range(1, grid_height - 1):
-		for i_num in range(1, grid_width - 1):
-			grid[i_row][i_num] += value
+	while True:
+		# stoppen met loopen als er niks nieuws flashed
+		keep_going = False
 
-# def change_all(grid, condition, value):
-# 	grid_height = len(grid)
-# 	grid_width = len(grid[0])
-# 	for i_row in range(1, grid_height - 1):
-# 		for i_num in range(1, grid_width - 1):
-# 			if grid[i_row][i_num] >= condition:
-# 				grid[i_row][i_num] = value
+		# save where change has happened
+		change = np.zeros((height, height), dtype=int)
 
-def surrounders(grid, x, y):
-	if grid[y][x - 1] != 0 and grid[y][x - 1] != -1:
-		grid[y][x - 1] += 1
-	if grid[y - 1][x - 1] != 0 and grid[y - 1][x - 1] != -1:
-		grid[y - 1][x - 1] += 1
-	if grid[y - 1][x] != 0 and grid[y - 1][x] != -1:
-		grid[y - 1][x] += 1
-	if grid[y - 1][x + 1] != 0 and grid[y - 1][x + 1] != -1:
-		grid[y - 1][x + 1] += 1
-	if grid[y][x + 1] != 0 and grid[y][x + 1] != -1:
-		grid[y][x + 1] += 1
-	if grid[y + 1][x + 1] != 0 and grid[y + 1][x + 1] != -1:
-		grid[y + 1][x + 1] += 1
-	if grid[y + 1][x] != 0 and grid[y + 1][x] != -1:
-		grid[y + 1][x] += 1
-	if grid[y + 1][x - 1] != 0 and grid[y + 1][x - 1] != -1:
-		grid[y + 1][x - 1] += 1
+		# door alle octo's heen loopen
+		for y, x in product(range(height), repeat=2):
+			# als octo nog niet geflashed is en octo is >9 flashen
+			if not flashed[y, x] and grid[y, x] > 9:
+				flashes += 1
+				# onthouden dat deze octo geflashed heeft
+				flashed[y, x] = True
+				# doorgaan omdat er nieuwe flash geweest is
+				keep_going = True
 
-def flash(grid, x, y):
-	grid[y][x] = 0
-	surrounders(grid, x, y)
-	print_2D(grid)
-	if grid[y][x - 1] >= 10:
-		flash(grid, y, x - 1)
-	if grid[y - 1][x - 1] >= 10:
-		flash(grid, y - 1, x - 1)
-	if grid[y - 1][x] >= 10:
-		flash(grid, y - 1, x)
-	if grid[y - 1][x + 1] >= 10:
-		flash(grid, y - 1, x + 1)
-	if grid[y][x + 1] >= 10:
-		flash(grid, y, x + 1)
-	if grid[y + 1][x + 1] >= 10:
-		flash(grid, y + 1, x + 1)
-	if grid[y + 1][x] >= 10:
-		flash(grid, y + 1, x)
-	if grid[y + 1][x - 1] >= 10:
-		flash(grid, y + 1, x - 1)
+				# omringende octos 1 incrementen
+				for dy, dx in product(range(-1, 2), repeat=2):
+					if dy == dx == 0:
+						continue
+					yy = y + dy
+					xx = x + dx
+					if not (0 <= yy < height and 0 <= xx < height):
+						continue
+					change[yy, xx] += 1
+		# increments bij huidige octo waarde optellen
+		grid += change
 
-
-def increment_adjacents(grid):
-	grid_height = len(grid)
-	grid_width = len(grid[0])
-	for y in range(1, grid_height - 1):
-		for x in range(1, grid_width - 1):
-			if grid[y][x] >= 10:
-				print("back")
-				flash(grid, x, y)
-
-def take_step(grid):
-	increment_all(grid, 1)
-	# change_all(grid, 10, 0)
-	increment_adjacents(grid)
-	flashes = count_2D(grid, 0)
+		# stoppen met loopen als er niks nieuws flashed
+		if not keep_going:
+			break
+	# geflashte octos aanpassen naar 0
+	for y, x in product(range(height), repeat=2):
+		if flashed[y, x]:
+			grid[y, x] = 0
 	return flashes
 
+def all_flashing(grid, height):
+	for y, x in product(range(height), repeat=2):
+		if grid[y, x] != 0:
+			return 0
+	return 1
+
 def simulate_steps(grid, steps):
+	height = len(grid)
 	flashes = 0
 	for step in range(steps):
-		flashes += take_step(grid)
-		print(f"After step {step + 1}")
-		print_2D(grid)
+		flashes += take_step(grid, height)
+		if all_flashing(grid, height):
+			print(f"After step {step + 1}")
+			break
+		# print(f"After step {step + 1}")
+		# print(grid)
 	return flashes
 
 if __name__ == "__main__":
-	sys.setrecursionlimit(500)
-	octos = load_data("sample.txt")
-	octos_bordered = add_borders(octos, -1)
-	print("Before any steps:")
-	print_2D(octos_bordered)
-	steps = 2
-	flashes = simulate_steps(octos_bordered, steps)
+	grid = load_data("input.txt")
+	# print(grid)
+	flashes = simulate_steps(grid, 1000)
 	print("flashes: ", flashes)
